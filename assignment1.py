8b324b2a10521fa3552b9c602c6b37735ca8f0ab
mofatima <mfatima@unomaha.edu>
2024-09-12 19:36:55 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -6,3 +6,12 @@ train_data = pd.read_csv(url_train)
 
 # Inspect the data
 print(train_data.head())TEST
+
+import statsmodels.api as sm
+
+# Set up the ARIMA model (p, d, q) based on the data
+model = sm.tsa.ARIMA(train_data['trips'], order=(1,1,1))
+modelFit = model.fit()
+
+# Print summary to inspect the results
+print(modelFit.summary())
