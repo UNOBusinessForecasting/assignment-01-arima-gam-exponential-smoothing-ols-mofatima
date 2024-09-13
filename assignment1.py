70adafd97b0058c939ed6efbdc2cc7c3727d9155
mofatima <mfatima@unomaha.edu>
2024-09-12 19:35:44 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,8 @@
-TEST
+import pandas as pd
+
+# Load the training data
+url_train = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
+train_data = pd.read_csv(url_train)
+
+# Inspect the data
+print(train_data.head())TEST
